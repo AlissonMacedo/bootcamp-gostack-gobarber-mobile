04a000eff7103c6df6dc5c4aa2b08e309d968ca2
Alisson <alissontecnico@gmail.com>
2019-08-28 09:30:02 -0300

    route login

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {StatusBar} from 'react-native';
 import './config/ReactotronConfig';
 
 import {store, persistor} from './store';
-import Routes from './routes';
+
+import App from '../App';
 
 export default function Index() {
   return (
@@ -13,7 +14,7 @@ export default function Index() {
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <Routes />
+          <App />
         </PersistGate>
       </Provider>
     </>
