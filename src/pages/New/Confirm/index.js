03944832708db00fe2052099e867b8cb36383562
Alisson <alissontecnico@gmail.com>
2019-08-29 17:24:57 -0300

    Confirmando agendamento

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,22 +1,22 @@
-import React, { useMemo } from 'react';
-import { formatRelative, parseISO } from 'date-fns';
+import React, {useMemo} from 'react';
+import {formatRelative, parseISO} from 'date-fns';
 import pt from 'date-fns/locale/pt';
-import { TouchableOpacity } from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '~/services/api';
 
 import Background from '~/components/Background';
 
-import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+import {Container, Avatar, Name, Time, SubmitButton} from './styles';
 
-export default function Confirm({ navigation }) {
+export default function Confirm({navigation}) {
   const provider = navigation.getParam('provider');
   const time = navigation.getParam('time');
 
   const dateFormatted = useMemo(
-    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
-    [time]
+    () => formatRelative(parseISO(time), new Date(), {locale: pt}),
+    [time],
   );
 
   async function handleAddAppointment() {
@@ -50,14 +50,13 @@ export default function Confirm({ navigation }) {
   );
 }
 
-Confirm.navigationOptions = ({ navigation }) => ({
+Confirm.navigationOptions = ({navigation}) => ({
   title: 'Confirmar agendamento',
   headerLeft: () => (
     <TouchableOpacity
       onPress={() => {
         navigation.goBack();
-      }}
-    >
+      }}>
       <Icon name="chevron-left" size={20} color="#FFF" />
     </TouchableOpacity>
   ),
