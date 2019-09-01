cd3955ae12227750758f6aade92c37e78d745841
Alisson <alissontecnico@gmail.com>
2019-08-31 22:31:26 -0300

    corect dateinput

--- src/components/DateInput/index.ios.js
+++ src/components/DateInput/index.ios.js
@@ -1,18 +1,18 @@
-import React, { useState, useMemo } from 'react';
-import { DatePickerIOS } from 'react-native';
-import { format } from 'date-fns';
+import React, {useState, useMemo} from 'react';
+import {DatePickerIOS} from 'react-native';
+import {format} from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, DateButton, DateText, Picker } from './styles';
+import {Container, DateButton, DateText, Picker} from './styles';
 
-export default function DateInput({ date, onChange }) {
+export default function DateInput({date, onChange}) {
   const [opened, setOpened] = useState(false);
 
   const dateFormatted = useMemo(
-    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
-    [date]
+    () => format(date, "dd 'de' MMMM 'de' yyyy", {locale: pt}),
+    [date],
   );
 
   return (
